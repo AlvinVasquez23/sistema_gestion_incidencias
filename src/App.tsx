e34d6ccbb85549332225128f4d196305cb9e2083
Alvin_Vasquez <alvinhvasquez@gmail.com>
2026-09-13 05:21:21 -0500

    fix: corregir importacion de Loader a loader en App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import CaptureShell, {
   CapturaHome, MisCapturas, ConsultaIncidencias, CapturaForm,
 } from './components/layout/CaptureShell'
 import AutoLogout from './components/seguridad/AutoLogout'
-import { Loader } from './components/Loader'
+import { Loader } from './components/loader'
 
 /* Solo supervisores ven la app completa; un auxiliar que intente entrar va a /captura */
 function SoloSupervisor({ children }: { children: ReactElement }) {
